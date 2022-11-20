e70c80684da68cb87a57e8f025cbf7c8ab139611
FURY <44730407+furyhm@users.noreply.github.com>
2022-11-21 01:42:01 +0300

    Add files via upload

---
+++ assete/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.websit-slider').slick({
+      dots : true,
+      slidesToShow : 3,
+      slidesToScroll : 3,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.websit-slider-2').slick({
+      dots : true,
+      slidesToShow : 3,
+    });
+  });
